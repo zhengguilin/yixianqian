9d14d8f7e7c77059515e410f4100462bee20fd0b
zhengguilin <490814592@qq.com>
2013-08-19 21:30:37 +0800

    修改

---
+++ src/com/yxq/dao/Action.java
@@ -0,0 +1,17 @@
+/**
+ * NewHeight.com Inc.
+ * Copyright (c) 2008-2010 All Rights Reserved.
+ */
+package com.yxq.dao;
+
+/**
+ * <pre>
+ * 
+ * </pre>
+ *
+ * @author zhengguilin
+ * @version $Id: Action.java, v 0.1 2013-8-19 下午9:28:47 zhengguilin Exp $
+ */
+public class Action {
+
+}
